c0834a7ca038a2e30e7a3115c81c6feac93d6456
Josh Pontillas <jpontillas@eccs.wa.edu.au>
2026-06-03 12:51:43 +0800

    Revert "feat(contact): add /contact page with mailto and tel links"
    This reverts commit 7348a4aefcf4fb00bbcf39f45efeecc844de2473.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index.tsx";
 import About from "./pages/About.tsx";
 import Leadership from "./pages/Leadership.tsx";
-import Contact from "./pages/Contact.tsx";
+import Resources from "./pages/Resources.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -23,7 +23,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
           <Route path="/leadership" element={<Leadership />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/resources" element={<Resources />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
